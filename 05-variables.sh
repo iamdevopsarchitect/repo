78f941e862a1e8efb3d8136093431de644037c04
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-01 00:07:49 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your username:"
+
+read USERNAME #TAKES INPUT INTO USERNAME variable
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password:"
+
+read -s PASSWORD
+echo "Password entered is: $PASSWORD"
