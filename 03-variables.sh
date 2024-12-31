ffbf1a71cc56a8dd769949f1f443dfcde4be44ed
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2024-12-31 21:57:17 +0530

    Adding Variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Person1=Kavi
+Person2=Krak
+echo "$Person1: Hi $Person2, how are you"
+echo "$Person2: Hi Angel, I am good. how are you doing ?"
+echo "$Person1: Iam doing good. will you marry me ?"
+echo "$Person2: I am obsolutely ready,"
