b1f3eb3bceb16b88e2b8e2198077a942ffb24717
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-01 23:23:48 +0530

    arrays

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#INDEX STARTS FROM 0, SIZE IS 3
+FRUITS=("APPLE" "KIWI" "ORANGES") #ARRAYS
+
+echo "FIRST FRUIT IS ${FRUITS[0]}"
+echo "Second FRUIT IS ${FRUITS[1]}"
+echo "Third FRUIT IS ${FRUITS[2]}"
+
+echo "First FRUIT IS ${FRUITS[@]}"
