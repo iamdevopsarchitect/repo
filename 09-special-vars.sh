878227580e9ee6718f795f9c0ec6d5a979d9a0e8
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-02 13:51:45 +0530

    spl variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -6,3 +6,5 @@ echo "Script name: $0"
 echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "PID of the script excuting now: $$"
+sleep 100 &
+echo "PID of last backgrond command: $!"
