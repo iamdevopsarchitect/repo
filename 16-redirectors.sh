cb09d968e18064c583604992ffcb4b43b5af8f7f
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-09 15:55:57 +0530

    REDIRECTORS

---
+++ 16-redirectors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -E "$R Please run this script with root privilages $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R Failed $N"  &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G Success $N"  &>>$LOG_FILE
+    fi
+
+}
+
+CHECK_ROOT
