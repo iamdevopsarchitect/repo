290540d2063b6a7a30fcc2e4bb98d7b759d4b191
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-09 16:30:31 +0530

    REDIRECTORS

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -31,24 +31,12 @@ VALIDATE(){
 
 }
 
-USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2..." 
-    exit 1
-}
-
-CHECK_ROOT(){
-    if [ $# -eq 0 ]
-    then
-        USAGE
-    fi
-}
-
 
 CHECK_ROOT
 
 for package in $@ # refers to all argumenrts passed to it
 do
-    dnf list installed $package  &>>$LOG_FILE
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
         echo "$package is not installed, going to install it.." &>>$LOG_FILE
