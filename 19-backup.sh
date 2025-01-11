c1a312618655ddf057cd11c23f2be3dabf24a997
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-11 14:39:19 +0530

    backup

--- 19-backup.sh
+++ 19-backup.sh
@@ -21,40 +21,40 @@ then
     exit 1
 fi
 
-# if [ ! -d $SOURCE_DIR ]
-# then
-#     echo "$SOURCE_DIR does not exist...Please check"
-# fi
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR does not exist...Please check"
+fi
 
-# if [ ! -d $DEST_DIR ]
-# then
-#     echo "$DEST_DIR does not exist...Please check"
-# fi
+if [ ! -d $DEST_DIR ]
+then
+    echo "$DEST_DIR does not exist...Please check"
+fi
 
-# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
 
-# echo "Files: $FILES"
+echo "Files: $FILES"
 
-# if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
-# then
-#     echo "Files are found"
-#     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-#     find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+then
+    echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
 
-#     #check if zip file is successfully created or not
-#     if [ -f $ZIP_FILE ]
-#     then
-#         echo "Successfully zippped files older than $DAYS"
-#         #remove the files after zipping
-#         while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-#         do
-#             echo "Deleting file: $file"
-#             rm -rf $file
-#         done <<< $FILES
-#     else
-#         echo "Zipping the files is failed"
-#         exit 1
-#     fi
-# else
-#     echo "No files older than $DAYS"
-# fi
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
+else
+    echo "No files older than $DAYS"
+fi
