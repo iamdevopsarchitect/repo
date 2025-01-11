81fa6af55fcc3633853540e115b84b22c5d4b642
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-11 13:54:53 +0530

    backup

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,3 +5,5 @@ echo "$Person1: Hi $Person2, how are you"
 echo "$Person2: Hi Angel, I am good. how are you doing ?"
 echo "$Person1: Iam doing good. will you marry me ?"
 echo "$Person2: I am obsolutely ready,"
+
+
