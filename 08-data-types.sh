14d47b4169c377b19018b6a12c8f671c767b67c8
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-01-15 23:28:31 +0530

    data types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,6 +5,6 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
+echo "First Number is $NUMBER1"
+echo "Second Number is $NUMBER2"
 echo "Total of $NUMBER1 and $NUMBER2 are:$SUM"
-echo $NUMBER1
-echo $NUMBER2
